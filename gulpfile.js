b654d4ec64b7a896fab78813d09409f91ca3a5d5
anton <agrivyuk>
2022-12-03 19:40:19 +0300

    add browser-sync to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 
 var gulp = require('gulp') // Подключаем Gulp
 const sass = require('gulp-sass')(require('sass')); // Подключаем Sass пакет
+var browserSync = require('browser-sync').create(); // Подключаем Browser-sync
+var reload      = browserSync.reload;
 
 gulp.task('sass', function() { // Создаем таск "sass"
     return gulp.src('./style/sass/**/*.sass', './style/sass/**/*.sass') // Берем источник
@@ -12,4 +14,16 @@ gulp.task('watch', function() {
     gulp.watch('./style/sass/**/*.sass', gulp.parallel('sass')); // Наблюдение за sass файлами в папке sass
 });
 
-gulp.task('default', gulp.series('watch'));
+gulp.task('serve', function () {
+
+    // Serve files from the root of this project
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("*.html").on("change", reload);
+});
+
+gulp.task('default', gulp.series('serve', 'watch'));
